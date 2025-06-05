288781bb17f1d1b6b843c40e4dbf32a3e5271fe5
Hugo Zuba <hamz02gh@gmail.com>
2025-06-05 20:38:51 -0300

    updated slide and navbar

--- index.php
+++ index.php
@@ -43,7 +43,8 @@ for ($x = 0; $x < count($carros); $x++) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TopGear</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
 
 </head>
@@ -51,18 +52,51 @@ for ($x = 0; $x < count($carros); $x++) {
 <body>
     <header>
         <div class="headContainer">
-            <div class="logoHead">Top<span>Gear</span></div>
-            <nav class="listaNav">
-                <ul>
-                    <li><a href="#" class="linkNav">Início</a></li>
-                    <li><a href="#" class="linkNav">Galeria</a></li>
-                    <li><a href="#" class="linkNav">Sobre</a></li>
-                    <li><a href="#" class="linkNav">Contato</a></li>
-                </ul>
-            </nav>
+            <div class="headNavbar">
+                <div class="logoHead">Top<span>Gear</span></div>
+                <nav class="listaNav">
+                    <ul>
+                        <li><a href="#" class="linkNav">Início</a></li>
+                        <li><a href="#" class="linkNav">Galeria</a></li>
+                        <li><a href="#" class="linkNav">Sobre</a></li>
+                        <li><a href="#" class="linkNav">Contato</a></li>
+                    </ul>
+                </nav>
+            </div>
+
         </div>
     </header>
     <main class="mainContainer">
+        <!-- Carousel with autoplay derived from bootstrap -->
+
+        <div class="content">
+            <h2>O que somos?</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus tempora veritatis saepe ipsam, porro necessitatibus rem sit iusto sint, corrupti officia aspernatur sapiente vitae ipsum voluptate. Consequatur dolores molestiae suscipit! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim fuga quisquam, quod commodi doloribus ea perspiciatis aliquam dicta laborum autem debitis libero nesciunt. Aliquam exercitationem laboriosam delectus illum accusamus architecto! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis laudantium vel, maxime minus fugiat possimus doloribus? Nemo magnam blanditiis voluptatem ratione cupiditate voluptates hic nobis! Earum magni distinctio praesentium deleniti? </p>
+        </div>
+
+        <div class="slideAuto">
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <?php foreach ($destaques as $carro): ?>
+                        <?php if ($carro['destaque'] == true): ?>
+                            <div class="carousel-item active">
+                                <img src="<?= $carro['capa'] ?>" class="d-block w-100" alt="<?= $carro['modelo'] ?>">
+                            </div>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
         <!-- 
         <section>
             <h2 class="sectionTitle">Galeria</h2>
@@ -87,27 +121,7 @@ for ($x = 0; $x < count($carros); $x++) {
                 -->
     </main>
 
-    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-            <?php foreach ($destaques as $carro) : ?>
-                <?php if ($carro['destaque']): ?>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                        <img src="<?= $carro['capa'] ?>" class="d-block w-100" alt="<?= $carro['modelo'] ?>">
-                    </div>
-                <?php endif; ?>
-            <?php endforeach; ?>
 
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
     <h2>kajdlçjafklçjfl</h2>
     <footer>
         <div class="container footerContent">
@@ -115,7 +129,9 @@ for ($x = 0; $x < count($carros); $x++) {
             <p class="copyright">@ <?= date('Y') ?> Todos os direitos reservados</p>
         </div>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+        crossorigin="anonymous"></script>
 
 
 </body>
