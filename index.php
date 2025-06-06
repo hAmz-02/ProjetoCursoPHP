fb744f44deb15b99686085d6a6ec4356b4d2ffd8
Hugo Zuba <hamz02gh@gmail.com>
2025-06-06 14:29:01 -0300

    low updates

--- index.php
+++ index.php
@@ -97,6 +97,7 @@ for ($x = 0; $x < count($carros); $x++) {
                 </button>
             </div>
         </div>
+        
         <!-- 
         <section>
             <h2 class="sectionTitle">Galeria</h2>
@@ -121,17 +122,16 @@ for ($x = 0; $x < count($carros); $x++) {
                 -->
     </main>
 
-
-    <h2>kajdlçjafklçjfl</h2>
     <footer>
-        <div class="container footerContent">
-            <div class="footerLogo">LocadoraHazus</div>
+        <div class="footerAlign">
+            <div class="container footerContent">
+            <div class="footerLogo">TopGear</div>
             <p class="copyright">@ <?= date('Y') ?> Todos os direitos reservados</p>
         </div>
+        </div>
+      
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 
 
 </body>
